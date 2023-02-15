98fda98454c65b2a4e981bd1648258c78b6433c7
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-02-15 10:01:16 +0100

    initial commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,11 @@
+import User from '../mongodb/models/users.js';
+
+const getAllUsers = async (req, res) => {};
+const createUser = async (req, res) => {};
+const getUserInfoByID = async (req, res) => {};
+
+export {
+    getAllUsers,
+    createUser,
+    getUserInfoByID
+}
